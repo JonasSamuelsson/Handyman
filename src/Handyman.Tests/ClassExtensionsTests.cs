2b907b2da3583d6f113744d67a9d793a56b8fca1
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-05-10 15:51:23 +0200

    class: IsNull & IsNotNull
    close #3

---
+++ src/Handyman.Tests/ClassExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+
+namespace Handyman.Tests
+{
+    public class ClassExtensionsTests
+    {
+        public void ShouldCheckIsNull()
+        {
+            default(object).IsNull().ShouldBe(true);
+            new object().IsNull().ShouldBe(false);
+        }
+
+        public void ShouldCheckIsNotNull()
+        {
+            default(object).IsNotNull().ShouldBe(false);
+            new object().IsNotNull().ShouldBe(true);
+        }
+    }
+}
---
+++ src/Handyman/ClassExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Handyman
+{
+    public static class ClassExtensions
+    {
+        public static bool IsNull<T>(this T instance) where T : class
+        {
+            return instance == null;
+        }
+
+        public static bool IsNotNull<T>(this T instance) where T : class
+        {
+            return !instance.IsNull();
+        }
+    }
+}
