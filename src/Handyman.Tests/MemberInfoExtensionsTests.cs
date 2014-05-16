c3a2975b5817e0f5736ab4ea73b61b8e4e5f07a6
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-05-16 21:46:31 +0200

    MemberInfo: Get<TAttribute> & Has<TAttribute>
    close #8

---
+++ src/Handyman.Tests/MemberInfoExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+
+namespace Handyman.Tests
+{
+    public class MemberInfoExtensionsTests
+    {
+        public void ShouldCheckIfMemberHasAttribute()
+        {
+            typeof(SuperType).Has<TestAttribute>().ShouldBe(true);
+            typeof(SubType).Has<TestAttribute>().ShouldBe(false);
+            typeof(SubType).Has<TestAttribute>(true).ShouldBe(true);
+        }
+
+        public void ShouldGetAttributes()
+        {
+            typeof(SuperType).Get<TestAttribute>().Any().ShouldBe(true);
+            typeof(SubType).Get<TestAttribute>().Any().ShouldBe(false);
+            typeof(SubType).Get<TestAttribute>(true).Any().ShouldBe(true);   
+        }
+
+        [TestAttribute]
+        private class SuperType { }
+        private class SubType : SuperType { }
+        private class TestAttribute : Attribute { }
+    }
+}
---
+++ src/Handyman/MemberInfoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Handyman
+{
+    public static class MemberInfoExtensions
+    {
+        public static IEnumerable<TAttribute> Get<TAttribute>(this MemberInfo member, bool inherits = false) where TAttribute : Attribute
+        {
+            return member.GetCustomAttributes(typeof(TAttribute), inherits).Cast<TAttribute>();
+        }
+
+        public static bool Has<TAttribute>(this MemberInfo member, bool inherits = false) where TAttribute : Attribute
+        {
+            return member.GetCustomAttributes(typeof(TAttribute), inherits).Any();
+        }
+    }
+}
