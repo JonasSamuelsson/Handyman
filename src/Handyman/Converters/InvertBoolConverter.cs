9cb903c55a975881b0122f346122a75574deb896
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-05-31 19:28:18 +0200

    InvertBoolConverter

---
+++ src/Handyman/Converters/InvertBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Handyman.Converters
+{
+    public class InvertBoolConverter : ConverterBase<bool, bool>
+    {
+        public override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+
+        public override bool ConvertBack(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+    }
+}
