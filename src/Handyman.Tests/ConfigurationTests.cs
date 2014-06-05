2fe49f04d61979c73f45a8e1db751793618d663f
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-06-05 10:28:19 +0200

    added tests for configuration

---
+++ src/Handyman.Tests/ConfigurationTests.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using System;
+using System.Globalization;
+
+namespace Handyman.Tests
+{
+    public class ConfigurationTests
+    {
+        public void FormatProviderShouldBeCurrentCulture()
+        {
+            Configuration.Reset();
+            Configuration.FormatProvider().ShouldBe(CultureInfo.CurrentCulture);
+        }
+
+        public void StringComparisonShouldBeCurrentCulture()
+        {
+            Configuration.Reset();
+            Configuration.StringComparison().ShouldBe(StringComparison.CurrentCulture);
+        }
+    }
+}
--- src/Handyman/Configuration.cs
+++ src/Handyman/Configuration.cs
@@ -5,7 +5,20 @@ namespace Handyman
 {
     public static class Configuration
     {
-        public static Func<DateTimeOffset> Now = () => DateTimeOffset.Now;
-        public static Func<IFormatProvider> FormatProvider = () => CultureInfo.CurrentCulture;
+        public static Func<DateTimeOffset> Now;
+        public static Func<IFormatProvider> FormatProvider;
+        public static Func<StringComparison> StringComparison;
+
+        static Configuration()
+        {
+            Reset();
+        }
+
+        public static void Reset()
+        {
+            Now = () => DateTimeOffset.Now;
+            FormatProvider = () => CultureInfo.CurrentCulture;
+            StringComparison = () => System.StringComparison.CurrentCulture;
+        }
     }
 }
