03c95b09f4a85e7d0e916f26a410e44fa29a8df0
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-06-05 10:07:30 +0200

    added explicit test convention

---
+++ src/Handyman.Tests/Convention.cs
@@ -0,0 +1,11 @@
+﻿namespace Handyman.Tests
+{
+    public class Convention : Fixie.Conventions.Convention
+    {
+        public Convention()
+        {
+            Classes.NameEndsWith("Tests");
+            Methods.Where(x => x.IsPublic);
+        }
+    }
+}
