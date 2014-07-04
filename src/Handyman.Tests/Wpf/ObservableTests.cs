03452f4821818d03d777c710781788ba57d2658d
Jonas Samuelsson <jonas.samuelsson@cab.se>
2014-07-04 17:23:06 +0200

    added Observable<T>

---
+++ src/Handyman.Tests/Wpf/ObservableTests.cs
@@ -0,0 +1,26 @@
+﻿using Handyman.Wpf;
+using Shouldly;
+
+namespace Handyman.Tests.Wpf
+{
+    public class ObservableTests
+    {
+        public void AssigningNewValueShouldTriggerPropertyChangedEvent()
+        {
+            var observable = new Observable<int>();
+            var changedProperty = string.Empty;
+            observable.PropertyChanged += (sender, args) => changedProperty = args.PropertyName;
+            observable.Value = 1;
+            changedProperty.ShouldBe("Value");
+        }
+
+        public void AssigningSameValueShouldTriggerPropertyChangedEvent()
+        {
+            var observable = new Observable<int>(1);
+            var propertyChanged = false;
+            observable.PropertyChanged += (sender, args) => propertyChanged = true;
+            observable.Value = 1;
+            propertyChanged.ShouldBe(false);
+        }
+    }
+}
---
+++ src/Handyman/Wpf/Observable.cs
@@ -0,0 +1,47 @@
+﻿using Handyman.Annotations;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Handyman.Wpf
+{
+    public class Observable<T> : INotifyPropertyChanged, IObservable<T>
+    {
+        private T _value;
+
+        public Observable(T value = default (T))
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get { return _value; }
+            set
+            {
+                if (EqualityComparer<T>.Default.Equals(value, _value)) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public interface IObservable<T> : IReadOnlyObservable<T>
+    {
+        new T Value { get; set; }
+    }
+
+    public interface IReadOnlyObservable<T>
+    {
+        T Value { get; }
+    }
+}
