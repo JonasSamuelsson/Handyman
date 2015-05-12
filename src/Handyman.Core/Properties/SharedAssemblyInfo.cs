1a006cc6b2751158b413b1546abd1784fcadfe94
Jonas Samuelsson <jonas@samuelsson.at>
2015-05-12 22:00:12 +0200

    v0.7.0

--- src/Handyman.Core/Properties/SharedAssemblyInfo.cs
+++ src/Handyman.Core/Properties/SharedAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Handyman")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Jonas Samuelsson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.6.0.*")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0.*")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
