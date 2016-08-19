8c5c22ea13318df0730f218f935c800f620c3666
Jonas Samuelsson <jonassamuelsson@outlook.com>
2016-08-19 20:49:45 +0200

    removed ComparableExtensions.IsBetween
    #19

--- src/Handyman.Tests/ComparableExtensionsTests.cs
+++ src/Handyman.Tests/ComparableExtensionsTests.cs
@@ -4,21 +4,6 @@ namespace Handyman.Tests.Core
 {
     public class ComparableExtensionsTests
     {
-        public void ShouldCheckIfValueIsBetween()
-        {
-            1.IsBetween(2, 4).ShouldBe(false);
-            2.IsBetween(2, 4).ShouldBe(false);
-            3.IsBetween(2, 4).ShouldBe(true);
-            4.IsBetween(2, 4).ShouldBe(false);
-            5.IsBetween(2, 4).ShouldBe(false);
-
-            1.1.IsBetween(1.2, 1.4).ShouldBe(false);
-            1.2.IsBetween(1.2, 1.4).ShouldBe(false);
-            1.3.IsBetween(1.2, 1.4).ShouldBe(true);
-            1.4.IsBetween(1.2, 1.4).ShouldBe(false);
-            1.5.IsBetween(1.2, 1.4).ShouldBe(false);
-        }
-
         public void ShouldCheckIfValueIsInRange()
         {
             1.IsInRange(2, 4).ShouldBe(false);
