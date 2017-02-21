bbac2c823b7a5e1d21a8a7de72d08c287314b34d
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-02-21 23:22:43 +0100

    renamed ClassExtensions to ObjectExtensions

---
+++ src/Handyman.Extensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Handyman.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull<T>(this T instance) where T : class
+        {
+            return instance == null;
+        }
+
+        public static bool IsNotNull<T>(this T instance) where T : class
+        {
+            return !instance.IsNull();
+        }
+    }
+}
