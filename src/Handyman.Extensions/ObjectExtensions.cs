c55a8cb84ed836f14f0309a24bd785835b13cc07
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-02-21 23:30:40 +0100

    added ObjectExtensions.Coalesce

--- src/Handyman.Extensions/ObjectExtensions.cs
+++ src/Handyman.Extensions/ObjectExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Handyman.Extensions
+﻿using System;
+
+namespace Handyman.Extensions
 {
     public static class ObjectExtensions
     {
@@ -10,6 +12,21 @@
         public static bool IsNotNull<T>(this T instance) where T : class
         {
             return !instance.IsNull();
+        }
+
+        public static T Coalesce<T>(this T value, Func<T, bool> predicate, T @default)
+        {
+            return predicate(value) ? value : @default;
+        }
+
+        public static T Coalesce<T>(this T value, Func<T, bool> predicate, Func<T> factory)
+        {
+            return predicate(value) ? value : factory();
+        }
+
+        public static T Coalesce<T>(this T value, Func<T, bool> predicate, Func<T, T> factory)
+        {
+            return predicate(value) ? value : factory(value);
         }
     }
 }
--- src/Handyman.Tests/Extensions/ObjectExtensionsTests.cs
+++ src/Handyman.Tests/Extensions/ObjectExtensionsTests.cs
@@ -19,5 +19,16 @@ namespace Handyman.Tests.Extensions
             default(object).IsNotNull().ShouldBe(false);
             new object().IsNotNull().ShouldBe(true);
         }
+
+        [Fact]
+        public void Coalesce()
+        {
+            2.Coalesce(i => i > 1, 3).ShouldBe(2);
+            2.Coalesce(i => i < 1, 3).ShouldBe(3);
+            2.Coalesce(i => i > 1, () => 3).ShouldBe(2);
+            2.Coalesce(i => i < 1, () => 3).ShouldBe(3);
+            2.Coalesce(i => i > 1, i => i + 1).ShouldBe(2);
+            2.Coalesce(i => i < 1, i => i + 1).ShouldBe(3);
+        }
     }
 }
