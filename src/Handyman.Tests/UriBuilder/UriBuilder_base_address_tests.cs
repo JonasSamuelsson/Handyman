c120ba6a6171476cdf6650c051ef85ba79752ef7
Jonas Samuelsson <jonassamuelsson@outlook.com>
2017-04-06 08:16:14 +0200

    UriBuilder added support form base addresses

---
+++ src/Handyman.Tests/UriBuilder/UriBuilder_base_address_tests.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Handyman.Tests.UriBuilder
+{
+    public class UriBuilder_base_address_tests
+    {
+        [Fact]
+        public void should_construct_uri_from_base_address_alone()
+        {
+            new Handyman.UriBuilder()
+               .BaseAddress("ftp://base.io/foo")
+               .ToString()
+               .ShouldBe("ftp://base.io/foo");
+        }
+
+        [Fact]
+        public void should_construct_uri_from_base_address_with_overrides()
+        {
+            new Handyman.UriBuilder()
+               .BaseAddress("ftp://base.io/foo")
+               .Scheme("https")
+               .Host("test.com")
+               .Path("bar")
+               .Query("a=1")
+               .QueryParams("b", 2)
+               .QueryParams("c")
+               .ToString()
+               .ShouldBe("https://test.com/foo/bar?a=1&b=2&c");
+        }
+    }
+}
