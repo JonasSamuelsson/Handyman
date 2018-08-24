f8a602947a0e3818adb495f6a65cacd8c0802278
Jonas Samuelsson <jonassamuelsson@outlook.com>
2018-08-24 10:28:56 +0200

    added mediator extension methods to publish & send without cancellation token

---
+++ src/Handyman.Mediator.Tests/MediatorExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Handyman.Mediator.Tests
+{
+    public class MediatorExtensionsTests
+    {
+        [Fact]
+        public async Task ShouldPublishEventWithoutCancellationToken()
+        {
+            var services = new ServiceProvider();
+            services.Add<IEventHandler<TestEvent>>(() => new TestEventHandler());
+            var @event = new TestEvent();
+            var tasks = new Mediator(services).Publish(@event);
+            await Task.WhenAll(tasks);
+            @event.Handeled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task ShouldSendRequestWithoutCancellationToken()
+        {
+            var services = new ServiceProvider();
+            services.Add<IRequestHandler<TestRequest, string>>(() => new TestRequestHandler());
+            (await new Mediator(services).Send(new TestRequest { Response = "success" })).ShouldBe("success");
+        }
+
+        private class TestEvent : IEvent
+        {
+            public bool Handeled { get; set; }
+        }
+
+        private class TestEventHandler : SynchronousEventHandler<TestEvent>
+        {
+            protected override void Handle(TestEvent @event)
+            {
+                @event.Handeled = true;
+            }
+        }
+
+        private class TestRequest : IRequest<string>
+        {
+            public string Response { get; set; }
+        }
+
+        private class TestRequestHandler : SynchronousRequestHandler<TestRequest, string>
+        {
+            protected override string Handle(TestRequest request, CancellationToken cancellationToken) => request.Response;
+        }
+    }
+}
---
+++ src/Handyman.Mediator/MediatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Handyman.Mediator
+{
+    public static class MediatorExtensions
+    {
+        public static IEnumerable<Task> Publish<TEvent>(this IMediator mediator, TEvent @event) where TEvent : IEvent
+        {
+            return mediator.Publish(@event, CancellationToken.None);
+        }
+
+        public static Task<TResponse> Send<TResponse>(this IMediator mediator, IRequest<TResponse> request)
+        {
+            return mediator.Send(request, CancellationToken.None);
+        }
+    }
+}
