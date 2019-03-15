f0baf0e5033d45f5372f806251d03a15d4b2ff0a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-03-15 16:15:06 +0100

    api versioning: added ExactMatchValidator

---
+++ src/Handyman.AspNetCore/src/Handyman.AspNetCore/ApiVersioning/ExactMatchValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Handyman.AspNetCore.ApiVersioning
+{
+    public class ExactMatchValidator : IApiVersionValidator
+    {
+        public bool Validate(string version, bool optional, StringValues validVersions, out string matchedVersion, out string error)
+        {
+            matchedVersion = null;
+            error = null;
+
+            if (optional && version == string.Empty)
+                return true;
+
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var i = 0; i < validVersions.Count; i++)
+            {
+                var validVersion = validVersions[i];
+                if (version != validVersion) continue;
+                matchedVersion = validVersion;
+                return true;
+            }
+
+            error = $"Invalid api version, valid version(s): {validVersions.Count}.";
+            return false;
+        }
+    }
+}
