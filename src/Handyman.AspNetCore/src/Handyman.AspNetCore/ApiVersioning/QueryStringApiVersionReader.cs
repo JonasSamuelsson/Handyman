636a8c1182a1c863743edb86465e468f8ce1b456
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-03-15 16:00:58 +0100

    api versioning: added QueryStringApiVersionReader

---
+++ src/Handyman.AspNetCore/src/Handyman.AspNetCore/ApiVersioning/QueryStringApiVersionReader.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Handyman.AspNetCore.ApiVersioning
+{
+    internal class QueryStringApiVersionReader : IApiVersionReader
+    {
+        public StringValues Read(HttpRequest request)
+        {
+            return request.Query["api-version"];
+        }
+    }
+}
