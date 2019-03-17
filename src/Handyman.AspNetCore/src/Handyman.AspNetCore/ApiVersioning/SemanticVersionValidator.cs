dba9a3471adad6f7c8a7bc5831ef9ef371bdcd1a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-03-17 23:31:09 +0100

    api versioning: added semantic version validation

---
+++ src/Handyman.AspNetCore/src/Handyman.AspNetCore/ApiVersioning/SemanticVersionValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Handyman.AspNetCore.ApiVersioning
+{
+    internal class SemanticVersionValidator : IApiVersionValidator
+    {
+        public bool Validate(string version, bool optional, StringValues validVersions, out string matchedVersion, out string error)
+        {
+            matchedVersion = null;
+            error = null;
+
+            var parserResult = SemanticVersionParser.Parse(validVersions);
+
+            if (version == string.Empty)
+            {
+                if (optional)
+                    return true;
+
+                error = parserResult.ValidationError;
+                return false;
+            }
+
+            if (!SemanticVersionParser.TryParse(version, out var semanticVersion))
+            {
+                error = parserResult.ValidationError;
+                return false;
+            }
+
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var i = 0; i < parserResult.DeclaredVersions.Length; i++)
+            {
+                var declaredVersion = parserResult.DeclaredVersions[i];
+
+                if (semanticVersion.Major != declaredVersion.SemanticVersion.Major)
+                    continue;
+
+                var comparison = SemanticVersionComparer.Default.Compare(semanticVersion, declaredVersion.SemanticVersion);
+
+                if (comparison > 0)
+                    continue;
+
+                matchedVersion = declaredVersion.String;
+                return true;
+            }
+
+            error = parserResult.ValidationError;
+            return false;
+        }
+    }
+}
---
+++ src/Handyman.AspNetCore/test/Handyman.AspNetCore.Tests/SemanticVersionValidatorTests.cs
@@ -0,0 +1,102 @@
+using Handyman.AspNetCore.ApiVersioning;
+using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Handyman.AspNetCore.Tests
+{
+    public class SemanticVersionValidatorTests
+    {
+        [Theory, MemberData(nameof(GetUnmatchingVersionShouldBeInvalidParams))]
+        public void UnmatchingVersionShouldBeInvalid(string version, bool optional, string[] validVersions)
+        {
+            new SemanticVersionValidator().Validate(version, optional, validVersions, out var matchedVersion, out var error)
+                .ShouldBeFalse();
+
+            matchedVersion.ShouldBeNull();
+        }
+
+        public static IEnumerable<object[]> GetUnmatchingVersionShouldBeInvalidParams()
+        {
+            var testCases = new[]
+            {
+                new object[] {"1.0-alpha.1", new []{"1.0-alpha"}},
+                new object[] {"1.0-beta", new []{"1.0-alpha"}},
+                new object[] {"1.0", new []{ "1.0-alpha" } },
+                new object[] {"1.1", new []{"1.0"}},
+                new object[] {"2.0", new []{"1.0"}},
+                new object[] {"2.0", new []{"1.1"}}
+            };
+
+            foreach (var optional in new[] { false, true })
+            {
+                foreach (var values in testCases)
+                {
+                    var @params = values.ToList();
+                    @params.Insert(1, optional);
+                    yield return @params.ToArray();
+                }
+            }
+        }
+
+        [Fact]
+        public void MissingOptionalVersionShouldBeValid()
+        {
+            var version = "";
+            var optional = true;
+            var validVersions = new[] { "1" };
+
+            new SemanticVersionValidator().Validate(version, optional, validVersions, out var matchedVersion, out var error)
+                .ShouldBeTrue();
+
+            matchedVersion.ShouldBeNull();
+        }
+
+        [Theory, MemberData(nameof(GetMatchingVersionShouldBeValidParams))]
+        public void MatchingVersionShouldBeValid(string version, bool optional, string[] validVersions, string expectedMatchedVersion)
+        {
+            new SemanticVersionValidator().Validate(version, optional, validVersions, out var matchedVersion, out var error)
+                .ShouldBeTrue();
+
+            matchedVersion.ShouldBe(expectedMatchedVersion);
+        }
+
+        public static IEnumerable<object[]> GetMatchingVersionShouldBeValidParams()
+        {
+            var testCases = new[]
+            {
+                new object[] {"1", new []{"1"}, "1"},
+                new object[] {"1", new []{"1.0"}, "1.0"},
+                new object[] {"1.0", new []{"1"}, "1"},
+                new object[] {"1-alpha", new []{"1-alpha"}, "1-alpha"},
+                new object[] {"1-alpha", new []{"1.0-alpha"}, "1.0-alpha"},
+                new object[] {"1.0-alpha", new []{"1-alpha"}, "1-alpha"},
+                new object[] {"1.0-alpha", new []{"1.0-alpha"}, "1.0-alpha"},
+            };
+
+            foreach (var optional in new[] { false, true })
+            {
+                foreach (var values in testCases)
+                {
+                    var @params = values.ToList();
+                    @params.Insert(1, optional);
+                    yield return @params.ToArray();
+                }
+            }
+        }
+
+        [Fact]
+        public void MissingRequiredVersionShouldNotBeValid()
+        {
+            var version = "";
+            var optional = false;
+            var validVersions = new[] { "1" };
+
+            new SemanticVersionValidator().Validate(version, optional, validVersions, out var matchedVersion, out var error)
+                .ShouldBeFalse();
+
+            matchedVersion.ShouldBeNull();
+        }
+    }
+}
