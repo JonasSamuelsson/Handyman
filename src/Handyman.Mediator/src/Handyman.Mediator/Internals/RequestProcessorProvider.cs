56d4e6db69da748f3b208ee862c07a2b3a67eb24
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-04-04 15:54:48 +0200

    mediator - refactoring

--- src/Handyman.Mediator/src/Handyman.Mediator/Internals/RequestProcessorProvider.cs
+++ src/Handyman.Mediator/src/Handyman.Mediator/Internals/RequestProcessorProvider.cs
@@ -9,12 +9,14 @@ namespace Handyman.Mediator.Internals
 
         internal static RequestProcessor<TResponse> GetRequestProcessor<TResponse>(IRequest<TResponse> request)
         {
-            return (RequestProcessor<TResponse>)Cache.GetOrAdd(request.GetType(), requestType =>
-            {
-                var responseType = typeof(TResponse);
-                var requestProcessorType = typeof(RequestProcessor<,>).MakeGenericType(requestType, responseType);
-                return Activator.CreateInstance(requestProcessorType);
-            });
+            return (RequestProcessor<TResponse>)Cache.GetOrAdd(request.GetType(), CreateRequestProcessor<TResponse>);
+        }
+
+        private static object CreateRequestProcessor<TResponse>(Type requestType)
+        {
+            var responseType = typeof(TResponse);
+            var requestProcessorType = typeof(RequestProcessor<,>).MakeGenericType(requestType, responseType);
+            return Activator.CreateInstance(requestProcessorType);
         }
     }
 }
