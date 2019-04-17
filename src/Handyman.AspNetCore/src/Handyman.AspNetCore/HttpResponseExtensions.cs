7698cc3458aa2869c8c675cae93e1039c86acc10
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-04-17 10:07:01 +0200

    added HttpResponse.SetETagHeader extension method to Handyman.AspNetCore

---
+++ src/Handyman.AspNetCore/src/Handyman.AspNetCore/HttpResponseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Handyman.AspNetCore
+{
+    public static class HttpResponseExtensions
+    {
+        public static void SetETagHeader(this HttpResponse response, string eTag)
+        {
+            response.Headers[HeaderNames.ETag] = eTag;
+        }
+    }
+}
