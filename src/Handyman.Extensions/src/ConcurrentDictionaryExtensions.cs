ba5bcfbc60f6ef8fdcc1060ae50e82bac7049166
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-05-24 08:11:32 +0200

    extensions: added ConcurrentDictionary AddOrUpdate & GetOrThrow

---
+++ src/Handyman.Extensions/src/ConcurrentDictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Handyman.Extensions
+{
+    public static class ConcurrentDictionaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            dictionary.AddOrUpdate(key, value, (_, __) => value);
+        }
+
+        public static TValue GetOrThrow<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ src/Handyman.Extensions/test/ConcurrentDictionaryTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.Extensions.Tests
+{
+    public class ConcurrentDictionaryTests
+    {
+        [Fact]
+        public void AddOrUpdate()
+        {
+            var dictionary = new ConcurrentDictionary<int,string>();
+
+            dictionary.AddOrUpdate(1, "one");
+
+            dictionary.GetOrThrow(1).ShouldBe("one");
+
+            dictionary.AddOrUpdate(1, "ONE");
+
+            dictionary.GetOrThrow(1).ShouldBe("ONE");
+        }
+
+        [Fact]
+        public void GetOrThrow()
+        {
+            var dictionary = new ConcurrentDictionary<int, string>();
+
+            Should.Throw<KeyNotFoundException>(() => dictionary.GetOrThrow(1));
+
+            dictionary.AddOrUpdate(1, "one");
+
+            dictionary.GetOrThrow(1).ShouldBe("one");
+        }
+    }
+}
