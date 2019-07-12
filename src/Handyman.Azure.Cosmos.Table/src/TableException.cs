b069e76118dd7b0836ef89b13f990cce32b1bbf1
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-07-12 02:00:48 +0200

    added TableResult extensions

---
+++ src/Handyman.Azure.Cosmos.Table/src/TableException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Handyman.Azure.Cosmos.Table
+{
+    public class TableException : Exception
+    {
+        public TableException(int httpStatusCode)
+        {
+            HttpStatusCode = httpStatusCode;
+        }
+
+        public int HttpStatusCode { get; }
+    }
+}
---
+++ src/Handyman.Azure.Cosmos.Table/src/TableResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace Handyman.Azure.Cosmos.Table
+{
+    public static class TableResultExtensions
+    {
+        public static void AssertSuccessStatusCode(this TableResult result)
+        {
+            if (result.IsSuccessStatusCode())
+                return;
+
+            throw new TableException(result.HttpStatusCode);
+        }
+
+        public static bool IsSuccessStatusCode(this TableResult result)
+        {
+            return result.HttpStatusCode < 400;
+        }
+    }
+}
