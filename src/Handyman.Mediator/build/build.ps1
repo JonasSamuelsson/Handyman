3baac09402099aaf4ec408d749c9dc3e43dff9b8
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-07-14 00:17:43 +0200

    updated Handyman.Mediator project layout & build script

--- src/Handyman.Mediator/build/build.ps1
+++ src/Handyman.Mediator/build/build.ps1
@@ -9,7 +9,11 @@ $ErrorActionPreference = 'Stop'
 
 Import-Module -Force -Scope Local "$PSScriptRoot/../../../build/common.psm1"
 
-$expression = "exec BuildTestPack -project Handyman.Mediator"
+$root = "$PSScriptRoot/.."
+$project = "$root/src/Handyman.Mediator.csproj"
+$testProject = "$root/tests/Handyman.Mediator.Tests.csproj"
+
+$expression = "exec BuildTestPack -project $project -testProject $testProject"
 
 if ($artifacts) {
     $expression += " -artifacts $artifacts"
@@ -17,10 +21,6 @@ if ($artifacts) {
 
 if ($buildCounter) {
     $expression += " -buildCounter $buildCounter"
-}
-
-if ($ci) {
-    $expression += " -ci"
 }
 
 invoke-expression $expression
