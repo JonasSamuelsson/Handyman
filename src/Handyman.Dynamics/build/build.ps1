f4d5bc90fd83f489706b238d7958f39a62a155ce
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-07-14 00:14:38 +0200

    updated Handyman.Dynamics project layout & build script

--- src/Handyman.Dynamics/build/build.ps1
+++ src/Handyman.Dynamics/build/build.ps1
@@ -9,7 +9,11 @@ $ErrorActionPreference = 'Stop'
 
 Import-Module -Force -Scope Local "$PSScriptRoot/../../../build/common.psm1"
 
-$expression = "exec BuildTestPack -project Handyman.Dynamics"
+$root = "$PSScriptRoot/.."
+$project = "$root/src/Handyman.Dynamics.csproj"
+$testProject = "$root/tests/Handyman.Dynamics.Tests.csproj"
+
+$expression = "exec BuildTestPack -project $project -testProject $testProject"
 
 if ($artifacts) {
     $expression += " -artifacts $artifacts"
@@ -17,10 +21,6 @@ if ($artifacts) {
 
 if ($buildCounter) {
     $expression += " -buildCounter $buildCounter"
-}
-
-if ($ci) {
-    $expression += " -ci"
 }
 
 invoke-expression $expression
