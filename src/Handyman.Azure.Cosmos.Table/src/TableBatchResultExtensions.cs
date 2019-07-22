1761c4e8f2f58ba39386142a5399904021452de6
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-07-22 22:21:14 +0200

    added TableBatchResultExtensions

---
+++ src/Handyman.Azure.Cosmos.Table/src/TableBatchResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System.Linq;
+
+namespace Handyman.Azure.Cosmos.Table
+{
+    public static class TableBatchResultExtensions
+    {
+        public static void EnsureSuccessStatusCode(this TableBatchResult batchResult)
+        {
+            batchResult.ForEach(x => x.EnsureSuccessStatusCode());
+        }
+
+        public static bool HasSuccessStatusCode(this TableBatchResult batchResult)
+        {
+            return batchResult.All(x => x.HasSuccessStatusCode());
+        }
+    }
+}
