2dd5daf4283cba6da44b0438acb97df10272c6e2
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-07-22 02:01:39 +0200

    added CloudTableClientExtensions.GetTableReference

---
+++ src/Handyman.Azure.Cosmos.Table/src/CloudTableClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace Handyman.Azure.Cosmos.Table
+{
+    public static class CloudTableClientExtensions
+    {
+        public static T GetTableReference<T>(this CloudTableClient client, string tableName)
+            where T : CloudTable
+        {
+            return (T)Activator.CreateInstance(typeof(T), tableName, client);
+        }
+    }
+}
