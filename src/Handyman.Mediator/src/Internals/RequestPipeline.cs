8c418a7703ce7f9abe65d7a02dcca4675407df36
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-08-09 00:48:18 +0200

    request pipeline performance improvements
    #51

--- src/Handyman.Mediator/src/Internals/RequestPipeline.cs
+++ src/Handyman.Mediator/src/Internals/RequestPipeline.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,14 +19,18 @@ namespace Handyman.Mediator.Internals
 
         protected Task<TResponse> Process(TRequest request, Providers providers, CancellationToken cancellationToken)
         {
-            var filters = providers.RequestFilterProvider.GetFilters<TRequest, TResponse>(providers.ServiceProvider).ToList();
-
-            filters.Sort(CompareFilters);
-
+            var filters = providers.RequestFilterProvider.GetFilters<TRequest, TResponse>(providers.ServiceProvider).ToListOptimized();
             var handler = providers.RequestHandlerProvider.GetHandler<TRequest, TResponse>(providers.ServiceProvider);
 
-            var index = 0;
-            var length = filters.Count;
+            return filters.Count != 0
+                ? Execute(filters, handler, request, cancellationToken)
+                : Execute(handler, request, cancellationToken);
+        }
+
+        private static Task<TResponse> Execute(List<IRequestFilter<TRequest, TResponse>> filters,
+            IRequestHandler<TRequest, TResponse> handler, TRequest request, CancellationToken cancellationToken)
+        {
+            filters.Sort(CompareFilters);
 
             var context = new RequestFilterContext<TRequest>
             {
@@ -34,17 +38,29 @@ namespace Handyman.Mediator.Internals
                 Request = request
             };
 
+            var index = 0;
+            var filterCount = filters.Count;
+
             return Execute();
 
             Task<TResponse> Execute()
             {
-                context.CancellationToken.ThrowIfCancellationRequested();
+                if (index < filterCount)
+                {
+                    context.CancellationToken.ThrowIfCancellationRequested();
 
-                if (index == length)
-                    return handler.Handle(context.Request, context.CancellationToken);
+                    return filters[index++].Execute(context, Execute);
+                }
 
-                return filters[index++].Execute(context, Execute);
+                return RequestPipeline<TRequest, TResponse>.Execute(handler, request, cancellationToken);
             }
+        }
+
+        private static Task<TResponse> Execute(IRequestHandler<TRequest, TResponse> handler, TRequest request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return handler.Handle(request, cancellationToken);
         }
 
         private static int CompareFilters(object x, object y)
