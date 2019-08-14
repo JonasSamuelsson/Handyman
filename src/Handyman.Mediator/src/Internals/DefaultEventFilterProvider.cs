aa333686da2f4ad30fd834f05b526b83586ca36a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-08-14 11:30:19 +0200

    added event filter provider attribute
    #51

--- src/Handyman.Mediator/src/Internals/DefaultEventFilterProvider.cs
+++ src/Handyman.Mediator/src/Internals/DefaultEventFilterProvider.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Handyman.Mediator.Internals
 {
@@ -9,6 +11,18 @@ namespace Handyman.Mediator.Internals
         private DefaultEventFilterProvider() { }
 
         public virtual IEnumerable<IEventFilter<TEvent>> GetFilters<TEvent>(ServiceProvider serviceProvider) where TEvent : IEvent
+        {
+            var attribute = typeof(TEvent).GetCustomAttributes<EventFilterProviderAttribute>(true).SingleOrDefault();
+
+            if (attribute == null)
+                return GetDefaultFilters<TEvent>(serviceProvider);
+
+            var filters = GetDefaultFilters<TEvent>(serviceProvider).ToListOptimized();
+            filters.AddRange(attribute.GetFilters<TEvent>(serviceProvider));
+            return filters;
+        }
+
+        private static IEnumerable<IEventFilter<TEvent>> GetDefaultFilters<TEvent>(ServiceProvider serviceProvider) where TEvent : IEvent
         {
             return (IEnumerable<IEventFilter<TEvent>>)serviceProvider.Invoke(typeof(IEnumerable<IEventFilter<TEvent>>));
         }
---
+++ src/Handyman.Mediator/tests/EventFilterProviderAttributeTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Maestro;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.Mediator.Tests
+{
+    public class EventFilterProviderAttributeTests
+    {
+        [Fact]
+        public async Task ShouldGetEventFilterViaAttribute()
+        {
+            var container = new Container();
+
+            var mediator = new Mediator(container.GetService);
+
+            var @event = new Event();
+
+            await mediator.Publish(@event);
+
+            @event.Strings.ShouldBe(new[] { "success" });
+        }
+
+        [CustomEventFilterProvider]
+        public class Event : IEvent
+        {
+            public List<string> Strings { get; set; } = new List<string>();
+        }
+
+        private class CustomEventFilterProviderAttribute : EventFilterProviderAttribute
+        {
+            public override IEnumerable<IEventFilter<TEvent>> GetFilters<TEvent>(ServiceProvider serviceProvider)
+            {
+                return new[] { new EventFilter<TEvent>(), };
+            }
+        }
+
+        private class EventFilter<TEvent> : IEventFilter<TEvent> where TEvent : IEvent
+        {
+            public Task Execute(EventFilterContext<TEvent> context, EventFilterExecutionDelegate next)
+            {
+                ((Event)(object)context.Event).Strings.Add("success");
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
