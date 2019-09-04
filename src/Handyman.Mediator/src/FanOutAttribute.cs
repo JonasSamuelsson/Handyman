4fb046af2bcbdeb3d258db8e65c57f828c5a0928
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-09-04 09:15:23 +0200

    added mediator FanOutAttribute
    #54

---
+++ src/Handyman.Mediator/src/FanOutAttribute.cs
@@ -0,0 +1,44 @@
+﻿using Handyman.Mediator.Internals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Handyman.Mediator
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FanOutAttribute : RequestHandlerProviderAttribute
+    {
+        public override IRequestHandler<TRequest, TResponse> GetHandler<TRequest, TResponse>(ServiceProvider serviceProvider)
+        {
+            var type = typeof(IEnumerable<IRequestHandler<TRequest, TResponse>>);
+            var handlers = ((IEnumerable<IRequestHandler<TRequest, TResponse>>)serviceProvider.Invoke(type)).ToListOptimized();
+
+            if (handlers.Count == 0)
+                throw new InvalidOperationException("Request handlers not found.");
+
+            return new RequestHandler<TRequest, TResponse>(handlers);
+        }
+
+        private class RequestHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+        {
+            private readonly IEnumerable<IRequestHandler<TRequest, TResponse>> _handlers;
+
+            public RequestHandler(IEnumerable<IRequestHandler<TRequest, TResponse>> handlers)
+            {
+                _handlers = handlers.ToListOptimized();
+            }
+
+            public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                var tasks = _handlers.Select(x => x.Handle(request, cts.Token)).ToList();
+                var task = await Task.WhenAny(tasks).ConfigureAwait(false);
+                var response = await task.ConfigureAwait(false);
+                cts.Cancel();
+                return response;
+            }
+        }
+    }
+}
---
+++ src/Handyman.Mediator/tests/FanOutAttributeTests.cs
@@ -0,0 +1,167 @@
+﻿using Maestro;
+using Shouldly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Handyman.Mediator.Tests
+{
+    public class FanOutAttributeTests
+    {
+        [Fact]
+        public async Task ShouldCallAllHandlers()
+        {
+            var handler1 = new RequestHandler(Task.FromResult("success"));
+            var handler2 = new RequestHandler(Task.FromResult("success"));
+
+            var container = new Container(x =>
+            {
+                x.Add<IRequestHandler<Request, string>>().Instance(handler1);
+                x.Add<IRequestHandler<Request, string>>().Instance(handler2);
+            });
+
+            var mediator = new Mediator(container.GetService);
+
+            await mediator.Send(new Request());
+
+            handler1.Executed.ShouldBeTrue();
+            handler2.Executed.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task ShouldReturnWhenAnyHandlerCompletes()
+        {
+            var cts1 = new TaskCompletionSource<string>();
+            var cts2 = new TaskCompletionSource<string>();
+
+            var handler1 = new RequestHandler(cts1.Task);
+            var handler2 = new RequestHandler(cts2.Task);
+
+            var container = new Container(x =>
+            {
+                x.Add<IRequestHandler<Request, string>>().Instance(handler1);
+                x.Add<IRequestHandler<Request, string>>().Instance(handler2);
+            });
+
+            var mediator = new Mediator(container.GetService);
+
+            var task = mediator.Send(new Request());
+
+            task.Status.ShouldNotBe(TaskStatus.RanToCompletion);
+
+            cts1.SetResult("1");
+
+            (await task).ShouldBe("1");
+
+            cts1 = new TaskCompletionSource<string>();
+            cts2 = new TaskCompletionSource<string>();
+
+            handler1 = new RequestHandler(cts1.Task);
+            handler2 = new RequestHandler(cts2.Task);
+
+            container = new Container(x =>
+            {
+                x.Add<IRequestHandler<Request, string>>().Instance(handler1);
+                x.Add<IRequestHandler<Request, string>>().Instance(handler2);
+            });
+
+            mediator = new Mediator(container.GetService);
+
+            task = mediator.Send(new Request());
+
+            task.Status.ShouldNotBe(TaskStatus.RanToCompletion);
+
+            cts2.SetResult("2");
+
+            (await task).ShouldBe("2");
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessEvenIfSomeHandlersFail()
+        {
+            var cts1 = new TaskCompletionSource<string>();
+            var cts2 = new TaskCompletionSource<string>();
+
+            var handler1 = new RequestHandler(cts1.Task);
+            var handler2 = new RequestHandler(cts2.Task);
+
+            var container = new Container(x =>
+            {
+                x.Add<IRequestHandler<Request, string>>().Instance(handler1);
+                x.Add<IRequestHandler<Request, string>>().Instance(handler2);
+            });
+
+            var mediator = new Mediator(container.GetService);
+
+            var task = mediator.Send(new Request());
+
+            cts1.SetException(new Exception());
+            cts2.SetResult("success");
+
+            (await task).ShouldBe("success");
+        }
+
+        [Fact]
+        public async Task OnceTheFirstHandlerCompletesOtherHandlersShouldGetACancellationSignal()
+        {
+            var cts1 = new TaskCompletionSource<string>();
+            var cts2 = new TaskCompletionSource<string>();
+
+            var handler1 = new RequestHandler(cts1.Task);
+            var handler2 = new RequestHandler(cts2.Task);
+
+            var container = new Container(x =>
+            {
+                x.Add<IRequestHandler<Request, string>>().Instance(handler1);
+                x.Add<IRequestHandler<Request, string>>().Instance(handler2);
+            });
+
+            var mediator = new Mediator(container.GetService);
+
+            mediator.Send(new Request());
+
+            cts1.SetResult("success");
+
+            await Task.Delay(10);
+
+            handler1.Cancelled.ShouldBeFalse();
+            handler2.Cancelled.ShouldBeTrue();
+        }
+
+        [FanOut]
+        private class Request : IRequest<string> { }
+
+        private class RequestHandler : IRequestHandler<Request, string>
+        {
+            private readonly Task<string> _task;
+
+            public RequestHandler(Task<string> task)
+            {
+                _task = task;
+            }
+
+            public bool Cancelled { get; set; }
+            public bool Executed { get; set; }
+
+            public async Task<string> Handle(Request request, CancellationToken cancellationToken)
+            {
+                Executed = true;
+
+                while (!_task.IsCompleted)
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await Task.Delay(1);
+                        continue;
+                    }
+
+                    Cancelled = true;
+                    return null;
+                }
+
+                return await _task;
+            }
+        }
+    }
+}
