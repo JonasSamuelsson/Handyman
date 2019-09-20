4984433848b8a74bbd34c5259b08b60207cf44c8
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-09-20 12:34:13 +0200

    added dependency injection registration policy convention

---
+++ src/Handyman.DependencyInjection/src/Conventions/RegistrationPolicyConvention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Handyman.DependencyInjection.Conventions
+{
+    public class RegistrationPolicyConvention : IConvention
+    {
+        public void Execute(IReadOnlyCollection<Type> types, IServiceCollection services)
+        {
+            foreach (var type in types)
+            {
+                foreach (var attribute in type.GetCustomAttributes<ServiceRegistrationPolicyAttribute>())
+                {
+                    attribute.GetServiceRegistrationPolicy().Register(type, services);
+                }
+            }
+        }
+    }
+}
---
+++ src/Handyman.DependencyInjection/tests/RegistrationPolicyTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.DependencyInjection.Tests
+{
+    public class RegistrationPolicyTests
+    {
+        [Fact]
+        public void ShouldRegisterServicesUsingRegistrationPolicy()
+        {
+            var services = new ServiceCollection();
+
+            services.Scan(_ =>
+            {
+                _.Types(GetType().GetNestedTypes(BindingFlags.NonPublic));
+                _.UseRegistrationPolicies();
+            });
+
+            services.BuildServiceProvider().GetRequiredService<IFoo>().ShouldBeOfType<Foo>();
+        }
+
+        private interface IFoo { }
+
+        [ServiceRegistrationPolicy(typeof(ServiceRegistrationPolicy))]
+        private class Foo : IFoo { }
+
+        private class ServiceRegistrationPolicy : IServiceRegistrationPolicy
+        {
+            public void Register(Type type, IServiceCollection services)
+            {
+                services.AddTransient(typeof(IFoo), type);
+            }
+        }
+    }
+}
