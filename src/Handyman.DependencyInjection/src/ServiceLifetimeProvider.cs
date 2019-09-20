346832db7a3e0d0a335b5ec8872f7a558b19d8ec
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-09-20 12:32:46 +0200

    added dependency injection default inplementations convention

---
+++ src/Handyman.DependencyInjection/src/ServiceLifetimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Handyman.DependencyInjection
+{
+    public class ServiceLifetimeProvider
+    {
+        public static ServiceLifetime GetLifetimeOrDefault(Type type, ServiceLifetime defaultLifetime)
+        {
+            return type.GetCustomAttribute<ServiceLifetimeAttribute>()?.Lifetime ?? defaultLifetime;
+        }
+    }
+}
---
+++ src/Handyman.DependencyInjection/tests/DefaultImplementationsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.DependencyInjection.Tests
+{
+    public class DefaultImplementationsTests
+    {
+        [Fact]
+        public void ShouldRegisterDefaultImplementations()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.Scan(scanner => scanner.Types(GetType().GetNestedTypes(BindingFlags.NonPublic)).RegisterDefaultImplementations());
+
+            serviceCollection.BuildServiceProvider().GetRequiredService<IFoo>().ShouldBeOfType<Foo>();
+        }
+
+        private interface IFoo { }
+
+        private class Foo : IFoo { }
+    }
+}
