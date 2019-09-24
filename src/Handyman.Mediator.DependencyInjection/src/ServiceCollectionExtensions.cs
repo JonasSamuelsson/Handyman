ef4c2ada09adb3fc209cb1dc07ab48a5495dd04a
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-09-24 10:44:49 +0200

    Handyman.Mediator.DependencyInjcetion v1.0.0-beta.190924.900

---
+++ src/Handyman.Mediator.DependencyInjection/src/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Handyman.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Handyman.Mediator.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddMediator(this IServiceCollection services, Assembly assembly)
+        {
+            services.AddMediator(assembly, delegate { });
+        }
+
+        public static void AddMediator(this IServiceCollection services, Assembly assembly, Action<Configuration> configure)
+        {
+            var config = new Configuration();
+
+            configure(config);
+
+            services.TryAddTransient<IMediator>(sp => new Mediator(sp.GetService, config));
+
+            services.Scan(_ =>
+            {
+                _.Assembly(assembly);
+                _.RegisterConcreteClassesOf(typeof(IEventFilter<>));
+                _.RegisterConcreteClassesOf(typeof(IEventHandler<>));
+                _.RegisterConcreteClassesOf(typeof(IExperimentEvaluator<,>));
+                _.RegisterConcreteClassesOf(typeof(IExperimentToggle<>));
+                _.RegisterConcreteClassesOf(typeof(IRequestFilter<,>));
+                _.RegisterConcreteClassesOf(typeof(IRequestHandler<,>));
+            });
+        }
+    }
+}
---
+++ src/Handyman.Mediator.DependencyInjection/tests/AddMediatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.Mediator.DependencyInjection.Tests
+{
+    public class AddMediatorTests
+    {
+        [Fact]
+        public void ShouldAddServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMediator(GetType().Assembly);
+
+            services.Any(x => x.ServiceType == typeof(IMediator)).ShouldBeTrue();
+        }
+    }
+}
