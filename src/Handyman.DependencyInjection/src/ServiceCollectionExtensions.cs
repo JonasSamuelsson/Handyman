12b74604d55f7262addb6eabf7d0de089d566875
Jonas Samuelsson <JonasSamuelsson@users.noreply.github.com>
2019-11-05 11:40:23 +0100

    enabled fluent service registration
    this is enabled by returning the service collection from AddDiagnostics & Scan

--- src/Handyman.DependencyInjection/src/ServiceCollectionExtensions.cs
+++ src/Handyman.DependencyInjection/src/ServiceCollectionExtensions.cs
@@ -5,16 +5,17 @@ namespace Handyman.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddDiagnostics(this IServiceCollection services)
+        public static IServiceCollection AddDiagnostics(this IServiceCollection services)
         {
-            services.AddSingleton<IDiagnostics>(new Diagnostics(services));
+            return services.AddSingleton<IDiagnostics>(new Diagnostics(services));
         }
 
-        public static void Scan(this IServiceCollection services, Action<IScanner> action)
+        public static IServiceCollection Scan(this IServiceCollection services, Action<IScanner> action)
         {
             var scanner = new Scanner();
             action(scanner);
             scanner.Execute(services);
+            return services;
         }
     }
 }
