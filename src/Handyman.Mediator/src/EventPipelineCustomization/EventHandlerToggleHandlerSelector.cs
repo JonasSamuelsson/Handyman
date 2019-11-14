f7161747c84aaff0072f9d22705f9539d84e3052
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-11-14 22:36:57 +0100

    added event handler toggle
    #61

---
+++ src/Handyman.Mediator/src/EventPipelineCustomization/EventHandlerToggleHandlerSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Handyman.Mediator.EventPipelineCustomization
+{
+    internal class EventHandlerToggleHandlerSelector<TEvent> : IEventHandlerSelector<TEvent>
+        where TEvent : IEvent
+    {
+        private readonly Type _toggledHandlerType;
+
+        public EventHandlerToggleHandlerSelector(Type toggledHandlerType)
+        {
+            _toggledHandlerType = toggledHandlerType;
+        }
+
+        public Type FallbackHandlerType { get; set; }
+
+        public async Task SelectHandlers(List<IEventHandler<TEvent>> handlers, IEventPipelineContext<TEvent> context)
+        {
+            var toggle = context.ServiceProvider.GetRequiredService<IEventHandlerToggle<TEvent>>();
+            var enabled = await toggle.IsEnabled(context).ConfigureAwait(false);
+
+            if (!enabled)
+            {
+                handlers.RemoveAll(x => x.GetType() == _toggledHandlerType);
+            }
+            else if (FallbackHandlerType != null)
+            {
+                handlers.RemoveAll(x => x.GetType() == FallbackHandlerType);
+            }
+        }
+    }
+}
---
+++ src/Handyman.Mediator/tests/EventPipelineCustomization/EventHandlerToggleTests.cs
@@ -0,0 +1,66 @@
+﻿using Handyman.Mediator.EventPipelineCustomization;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Handyman.Mediator.Tests.EventPipelineCustomization
+{
+    public class EventHandlerToggleTests
+    {
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task ShouldToggleEventHandler(bool toggleEnabled)
+        {
+            var toggle = new EventHandlerToggle { Enabled = toggleEnabled };
+            var toggledHandler = new ToggledEventHandler();
+            var fallbackHandler = new FallbackEventHandler();
+
+            var services = new ServiceCollection();
+
+            services.AddScoped<IMediator>(x => new Mediator(x));
+            services.AddSingleton<IEventHandlerToggle<Event>>(toggle);
+            services.AddSingleton<IEventHandler<Event>>(toggledHandler);
+            services.AddSingleton<IEventHandler<Event>>(fallbackHandler);
+
+            await services.BuildServiceProvider().GetService<IMediator>().Publish(new Event());
+
+            toggledHandler.Executed.ShouldBe(toggleEnabled);
+            fallbackHandler.Executed.ShouldBe(!toggleEnabled);
+        }
+
+        [EventHandlerToggle(typeof(ToggledEventHandler), FallbackHandlerType = typeof(FallbackEventHandler))]
+        private class Event : IEvent { }
+
+        private class ToggledEventHandler : EventHandler<Event>
+        {
+            public bool Executed { get; set; }
+
+            protected override void Handle(Event @event)
+            {
+                Executed = true;
+            }
+        }
+
+        private class FallbackEventHandler : EventHandler<Event>
+        {
+            public bool Executed { get; set; }
+
+            protected override void Handle(Event @event)
+            {
+                Executed = true;
+            }
+        }
+
+        private class EventHandlerToggle : IEventHandlerToggle<Event>
+        {
+            public bool Enabled { get; set; }
+
+            public Task<bool> IsEnabled(IEventPipelineContext<Event> context)
+            {
+                return Task.FromResult(Enabled);
+            }
+        }
+    }
+}
