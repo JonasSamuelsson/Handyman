555dc7666917475a45da45b4ca59c2e335203428
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-11-14 16:02:51 +0100

    added type visibility tests

---
+++ src/Handyman.Mediator/tests/TypeVisibilityTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Handyman.Mediator.Tests
+{
+    public class TypeVisibilityTests
+    {
+        [Fact]
+        public void TypesWithFullNameContainingInternalShouldNotBePublic()
+        {
+            var errors = typeof(IMediator).Assembly.GetTypes()
+                .Where(x => x.IsPublic)
+                .Where(x => x.FullName.ToLowerInvariant().Contains("internal"))
+                .Select(x => x.FullName)
+                .ToList();
+
+            if (!errors.Any())
+                return;
+
+            throw new Exception(string.Join(Environment.NewLine, errors));
+        }
+
+        [Fact]
+        public void NestedTypesShouldNotBePublic()
+        {
+            var errors = typeof(IMediator).Assembly.GetTypes()
+                .Where(x => x.IsNestedPublic)
+                .Select(x => x.FullName)
+                .ToList();
+
+            if (!errors.Any())
+                return;
+
+            throw new Exception(string.Join(Environment.NewLine, errors));
+        }
+    }
+}
