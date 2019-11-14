ffa241db1c9a9cf0d77a3e9a2a929586b328c8fc
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-11-14 22:36:57 +0100

    added event handler toggle
    #61

---
+++ src/Handyman.Mediator/src/EventPipelineCustomization/EventHandlerToggleAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Handyman.Mediator.EventPipelineCustomization
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class EventHandlerToggleAttribute : EventPipelineBuilderAttribute
+    {
+        private readonly Type _toggledHandlerType;
+
+        public EventHandlerToggleAttribute(Type toggledHandlerType)
+        {
+            _toggledHandlerType = toggledHandlerType ?? throw new ArgumentNullException(nameof(toggledHandlerType));
+        }
+
+        public Type FallbackHandlerType { get; set; }
+
+        public override void Configure<TEvent>(IEventPipelineBuilder<TEvent> builder, ServiceProvider serviceProvider)
+        {
+            builder.AddHandlerSelector(new EventHandlerToggleHandlerSelector<TEvent>(_toggledHandlerType)
+            {
+                FallbackHandlerType = FallbackHandlerType
+            });
+        }
+    }
+}
---
+++ src/Handyman.Mediator/src/EventPipelineCustomization/IEventHandlerToggle.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Handyman.Mediator.EventPipelineCustomization
+{
+    public interface IEventHandlerToggle<TEvent>
+        where TEvent : IEvent
+    {
+        Task<bool> IsEnabled(IEventPipelineContext<TEvent> context);
+    }
+}
