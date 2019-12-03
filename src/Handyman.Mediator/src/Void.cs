3dab077e6e37984bee63adbe66420bc535c14434
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-12-03 17:16:06 +0100

    added mediator void equality comparers

--- src/Handyman.Mediator/src/Void.cs
+++ src/Handyman.Mediator/src/Void.cs
@@ -4,14 +4,29 @@
     {
         public static readonly Void Instance = new Void();
 
+        public bool Equals(Void other)
+        {
+            return true;
+        }
+
         public override bool Equals(object obj)
         {
-            return obj is Void;
+            return obj is Void other && Equals(other);
         }
 
         public override int GetHashCode()
         {
             return 0;
+        }
+
+        public static bool operator ==(Void left, Void right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Void left, Void right)
+        {
+            return !left.Equals(right);
         }
 
         public override string ToString()
