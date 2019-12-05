f5644ff746d9758d1a73a4405563b7a05258ef16
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-12-05 17:20:56 +0100

    added service validation
    #74

---
+++ src/Handyman.Mediator/tests/ServiceValidatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Handyman.Mediator.RequestPipelineCustomization;
+using Shouldly;
+using Xunit;
+
+namespace Handyman.Mediator.Tests
+{
+    public class ServiceValidatorTests
+    {
+        [Fact]
+        public void RequestWithSingleHandlerShouldPass()
+        {
+            var serviceTypes = new[]
+            {
+                typeof(IRequestHandler<DefaultPipelineRequest, Void>)
+            };
+
+            new ServiceValidator().Validate(serviceTypes);
+        }
+
+        [Fact]
+        public void RequestWithMultipleHandlersAndDefaultPipelineShouldFail()
+        {
+            var serviceTypes = new[]
+            {
+                typeof(IRequestHandler<DefaultPipelineRequest, Void>),
+                typeof(IRequestHandler<DefaultPipelineRequest, Void>)
+            };
+
+            Should.Throw<Exception>(() => new ServiceValidator().Validate(serviceTypes));
+        }
+
+        [Fact]
+        public void RequestWithMultipleHandlersAndCustomPipelineShouldPass()
+        {
+            var serviceTypes = new[]
+            {
+                typeof(IRequestHandler<CustomizedPipelineRequest, Void>),
+                typeof(IRequestHandler<CustomizedPipelineRequest, Void>)
+            };
+
+            new ServiceValidator().Validate(serviceTypes);
+        }
+
+        private class DefaultPipelineRequest : IRequest { }
+
+        [PipelineBuilder]
+        private class CustomizedPipelineRequest : IRequest { }
+
+        private class PipelineBuilderAttribute : RequestPipelineBuilderAttribute
+        {
+            public override void Configure<TRequest, TResponse>(IRequestPipelineBuilder<TRequest, TResponse> builder, IServiceProvider serviceProvider)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
