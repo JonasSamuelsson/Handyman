4045ce4f633840d13b0f6b61545bde8d6da22acc
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-12-09 22:12:33 +0100

    renamed diagnostics to service provider insights & simplified api by extracting extension method

--- src/Handyman.DependencyInjection/src/ServiceCollectionExtensions.cs
+++ src/Handyman.DependencyInjection/src/ServiceCollectionExtensions.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Handyman.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddDiagnostics(this IServiceCollection services)
+        public static IServiceCollection AddServiceProviderInsights(this IServiceCollection services)
         {
-            return services.AddSingleton<IDiagnostics>(new Diagnostics(services));
+            return services.AddSingleton<IServiceProviderInsights>(new ServiceProviderInsights(services));
         }
 
         public static IServiceCollection Scan(this IServiceCollection services, Action<IScanner> action)
