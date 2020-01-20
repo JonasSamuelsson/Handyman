f05516839642a1e847958850aeeaded138b8d86e
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-01-20 07:20:37 +0100

    outdated pipeline

---
+++ src/Handyman.Tools.Outdated/build/build.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [string]$artifacts = $null,
+    [string]$buildCounter
+)
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Force -Scope Local "$PSScriptRoot/../../../build/common.psm1"
+
+$project = "$PSScriptRoot/../src/Handyman.Tools.Ado.csproj"
+
+if (!$artifacts) {
+    $artifacts = [System.IO.Path]::Combine($project, 'bin', '.artifacts')
+}
+
+exec BuildTestPack -artifacts $artifacts -buildCounter $buildCounter -project $project
+
+write-host -f green 'script completed'
