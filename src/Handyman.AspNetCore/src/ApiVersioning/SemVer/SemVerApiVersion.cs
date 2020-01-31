e1eebe2e027ad0ed01fbc9007a1dc7cb1afe8956
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-01-31 11:39:03 +0100

    added SemVerApiVersion type

---
+++ src/Handyman.AspNetCore/src/ApiVersioning/SemVer/SemVerApiVersion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Handyman.AspNetCore.ApiVersioning.SemVer
+{
+    internal sealed class SemVerApiVersion : IApiVersion
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public object[] PreReleaseLabels { get; set; }
+
+        public bool IsPreRelease => PreReleaseLabels.Length != 0;
+
+        public string Text => IsPreRelease
+            ? $"{Major}.{Minor}-{string.Join(".", PreReleaseLabels)}"
+            : $"{Major}.{Minor}";
+
+        internal SemVerApiVersion(int major, int minor, object[] preReleaseLabels)
+        {
+            Major = major;
+            Minor = minor;
+            PreReleaseLabels = preReleaseLabels ?? throw new ArgumentNullException(nameof(preReleaseLabels));
+        }
+
+        public int CompareTo(IApiVersion other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMatch(IApiVersion other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
