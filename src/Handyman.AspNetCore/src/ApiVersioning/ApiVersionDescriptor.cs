4d989892df06031d7ddf5cb99949b67291cb43df
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-02-03 09:58:38 +0100

    added ApiVersionDescriptor & provider

---
+++ src/Handyman.AspNetCore/src/ApiVersioning/ApiVersionDescriptor.cs
@@ -0,0 +1,10 @@
+﻿namespace Handyman.AspNetCore.ApiVersioning
+{
+    internal class ApiVersionDescriptor
+    {
+        public IApiVersion[] Versions { get; set; }
+        public bool Optional { get; set; }
+        public string DefaultVersion { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
