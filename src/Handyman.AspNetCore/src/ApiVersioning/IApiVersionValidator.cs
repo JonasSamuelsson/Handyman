72ef45df7bce2e740bca0355b5db3f4fa90ffa75
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-02-12 11:37:06 +0100

    deleted old code

--- src/Handyman.AspNetCore/src/ApiVersioning/IApiVersionValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Primitives;
-
-namespace Handyman.AspNetCore.ApiVersioning
-{
-    public interface IApiVersionValidator
-    {
-        bool Validate(string version, bool optional, StringValues validVersions, out string matchedVersion, out string error);
-    }
-}
