cba5deec992be42b5affa3f7b9ae8e69733d5542
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-02-18 08:09:20 +0100

    added literal api version scheme

---
+++ src/Handyman.AspNetCore/src/ApiVersioning/Schemes/Literal/LiteralApiVersion.cs
@@ -0,0 +1,17 @@
+﻿namespace Handyman.AspNetCore.ApiVersioning.Schemes.Literal
+{
+    internal class LiteralApiVersion : IApiVersion
+    {
+        public LiteralApiVersion(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
+
+        public bool IsMatch(IApiVersion other)
+        {
+            return other is LiteralApiVersion apiVersion && apiVersion.Text == Text;
+        }
+    }
+}
