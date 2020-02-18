3a9bf49854904e57917d7580a48e2e84c6f48097
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-02-18 08:35:15 +0100

    added versioning scheme configuration api

---
+++ src/Handyman.AspNetCore/src/ApiVersioning/ApiVersioningOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Handyman.AspNetCore.ApiVersioning.Schemes.Literal;
+
+namespace Handyman.AspNetCore.ApiVersioning
+{
+    public class ApiVersioningOptions
+    {
+        internal Type ApiVersionParserType { get; set; }
+
+        public void UseLiteralApiVersioningScheme()
+        {
+            ApiVersionParserType = typeof(LiteralApiVersionParser);
+        }
+    }
+}
--- src/Handyman.AspNetCore/src/ApiVersioning/ServiceCollectionExtensions.cs
+++ src/Handyman.AspNetCore/src/ApiVersioning/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 
 namespace Handyman.AspNetCore.ApiVersioning
 {
@@ -12,14 +13,23 @@ namespace Handyman.AspNetCore.ApiVersioning
     {
         public static IServiceCollection AddApiVersioning(this IServiceCollection services)
         {
+            return services.AddApiVersioning(delegate { });
+        }
+
+        public static IServiceCollection AddApiVersioning(this IServiceCollection services, Action<ApiVersioningOptions> configureAction)
+        {
+            var options = new ApiVersioningOptions { ApiVersionParserType = typeof(MajorMinorPreReleaseApiVersionParser) };
+
+            configureAction.Invoke(options);
+
             services.TryAddSingleton<IActionDescriptorProvider, ApiVersionDescriptorProvider>();
-            services.TryAddSingleton<IApiVersionParser, MajorMinorPreReleaseApiVersionParser>();
+            services.TryAddSingleton(typeof(IApiVersionParser), options.ApiVersionParserType);
             services.TryAddSingleton<IApiVersionReader, QueryStringApiVersionReader>();
 
-#if NETSTANDARD2_0
-            services.TryAddSingleton<ApiVersionValidatorFilter>();
-#else
+#if NETCOREAPP3_0
             services.TryAddSingleton<MatcherPolicy, Routing.ApiVersionEndpointMatcherPolicy>();
+#else
+            services.TryAddSingleton<ApiVersionValidatorFilter>();
 #endif
 
             return services;
