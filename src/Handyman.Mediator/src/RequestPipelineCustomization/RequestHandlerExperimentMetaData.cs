974e6ea167b56a868aa0d57076913811db01fa52
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-16 00:20:46 +0200

    made all setters internal in RequestHandlerExperimentMetadata
    #96

--- src/Handyman.Mediator/src/RequestPipelineCustomization/RequestHandlerExperimentMetaData.cs
+++ src/Handyman.Mediator/src/RequestPipelineCustomization/RequestHandlerExperimentMetaData.cs
@@ -8,6 +8,6 @@ namespace Handyman.Mediator.RequestPipelineCustomization
     {
         public Type BaselineHandlerType { get; internal set; }
         public string Name { get; internal set; }
-        public IEnumerable<string> Tags { get; set; }
+        public IEnumerable<string> Tags { get; internal set; }
     }
 }
