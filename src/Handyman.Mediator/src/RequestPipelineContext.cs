5272de171f53f0b52a45565982403052bf3de430
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-15 23:59:42 +0200

    RequestPipelineContext implements IPipelineContext
    #94

--- src/Handyman.Mediator/src/RequestPipelineContext.cs
+++ src/Handyman.Mediator/src/RequestPipelineContext.cs
@@ -3,10 +3,12 @@ using System.Threading;
 
 namespace Handyman.Mediator
 {
-    public class RequestPipelineContext<TRequest>
+    public class RequestPipelineContext<TRequest> : IPipelineContext
     {
         public CancellationToken CancellationToken { get; set; }
         public TRequest Request { get; set; }
         public IServiceProvider ServiceProvider { get; set; }
+
+        object IPipelineContext.Message => Request;
     }
 }
