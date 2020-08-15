449fd01122fa777e954dd81cd0f6adaaf058931b
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-15 23:58:15 +0200

    RequestFilterToggleMetaData implements IToggleMetaData
    #94

--- src/Handyman.Mediator/src/RequestPipelineCustomization/RequestFilterToggleMetaData.cs
+++ src/Handyman.Mediator/src/RequestPipelineCustomization/RequestFilterToggleMetaData.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Handyman.Mediator.PipelineCustomization;
+using System;
 using System.Collections.Generic;
 
 namespace Handyman.Mediator.RequestPipelineCustomization
 {
-    public class RequestFilterToggleMetaData
+    public class RequestFilterToggleMetaData : IToggleMetaData
     {
         public string Name { get; internal set; }
         public IEnumerable<string> Tags { get; internal set; }
