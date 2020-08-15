3c6692274735b28aa4ccd28246a631e53f52d2a6
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-15 23:52:23 +0200

    ToggleBase implements IEventFilterToggle
    #94

--- src/Handyman.Mediator/src/PipelineCustomization/ToggleBase.cs
+++ src/Handyman.Mediator/src/PipelineCustomization/ToggleBase.cs
@@ -1,5 +1,15 @@
-﻿namespace Handyman.Mediator.PipelineCustomization
+﻿using Handyman.Mediator.EventPipelineCustomization;
+using System.Threading.Tasks;
+
+namespace Handyman.Mediator.PipelineCustomization
 {
-    public abstract class ToggleBase
-    {}
+    public abstract class ToggleBase : IEventFilterToggle
+    {
+        public virtual Task<bool> IsEnabled<TEvent>(EventFilterToggleMetaData toggleMetaData, EventPipelineContext<TEvent> context) where TEvent : IEvent
+        {
+            return IsEnabled((IToggleMetaData)toggleMetaData, context);
+        }
+
+        protected abstract Task<bool> IsEnabled(IToggleMetaData metaData, IPipelineContext context);
+    }
 }
