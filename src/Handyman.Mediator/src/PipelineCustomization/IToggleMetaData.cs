c8721fbfc68ddaff2b15ae44e369e9d5387607e4
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-15 23:10:12 +0200

    added IToggleMetaData interface

---
+++ src/Handyman.Mediator/src/PipelineCustomization/IToggleMetaData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Handyman.Mediator.PipelineCustomization
+{
+    public interface IToggleMetaData
+    {
+        string Name { get; }
+        IEnumerable<string> Tags { get; }
+    }
+}
