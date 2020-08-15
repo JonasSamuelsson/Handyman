f97a77d3ef44e6629d1a5959d8b7f45adb139b1c
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-15 23:41:59 +0200

    added IPipelineContext
    #94

---
+++ src/Handyman.Mediator/src/IPipelineContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+namespace Handyman.Mediator
+{
+    public interface IPipelineContext
+    {
+        CancellationToken CancellationToken { get; }
+        object Message { get; }
+        IServiceProvider ServiceProvider { get; }
+    }
+}
