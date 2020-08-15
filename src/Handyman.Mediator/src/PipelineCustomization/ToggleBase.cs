dfd684f90dd33822178bbce9ff1262b0c0e4a565
Jonas Samuelsson <jonas.samuelsson@cab.se>
2020-08-16 00:02:45 +0200

    ToggleBase implements IRequestHandlerToggle
    #94

--- src/Handyman.Mediator/src/PipelineCustomization/ToggleBase.cs
+++ src/Handyman.Mediator/src/PipelineCustomization/ToggleBase.cs
@@ -1,9 +1,10 @@
 ﻿using Handyman.Mediator.EventPipelineCustomization;
+using Handyman.Mediator.RequestPipelineCustomization;
 using System.Threading.Tasks;
 
 namespace Handyman.Mediator.PipelineCustomization
 {
-    public abstract class ToggleBase : IEventFilterToggle, IEventHandlerToggle
+    public abstract class ToggleBase : IEventFilterToggle, IEventHandlerToggle, IRequestHandlerToggle
     {
         public virtual Task<bool> IsEnabled<TEvent>(EventFilterToggleMetaData toggleMetaData, EventPipelineContext<TEvent> context) where TEvent : IEvent
         {
@@ -13,6 +14,11 @@ namespace Handyman.Mediator.PipelineCustomization
         public virtual Task<bool> IsEnabled<TEvent>(EventHandlerToggleMetaData toggleMetaData, EventPipelineContext<TEvent> context) where TEvent : IEvent
         {
             return IsEnabled((IToggleMetaData)toggleMetaData, context);
+        }
+
+        public virtual Task<bool> IsEnabled<TRequest, TResponse>(RequestHandlerToggleMetaData toggleMetaData, RequestPipelineContext<TRequest> context) where TRequest : IRequest<TResponse>
+        {
+            return IsEnabled(toggleMetaData, context);
         }
 
         protected abstract Task<bool> IsEnabled(IToggleMetaData metaData, IPipelineContext context);
