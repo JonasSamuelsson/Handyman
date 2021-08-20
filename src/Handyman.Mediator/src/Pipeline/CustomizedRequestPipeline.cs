51ff61532a457d7d1905d8170793877dde7cdbbf
Jonas Samuelsson <jonas.samuelsson@cab.se>
2021-08-20 15:22:29 +0200

    pipeline concurrency refactoring

--- src/Handyman.Mediator/src/Pipeline/CustomizedRequestPipeline.cs
+++ src/Handyman.Mediator/src/Pipeline/CustomizedRequestPipeline.cs
@@ -11,42 +11,60 @@ namespace Handyman.Mediator.Pipeline
 
         internal override async Task<TResponse> Execute(RequestContext<TRequest> requestContext)
         {
+            var pipelineBuilderContext = CreatePipelineBuilderContext(requestContext);
+
+            await ExecutePipelineBuilders(pipelineBuilderContext, requestContext);
+
+            AssertThereIsSingleHandlerToExecute(pipelineBuilderContext.Handlers);
+
+            return await ExecutePipeline(pipelineBuilderContext, requestContext);
+        }
+
+        private static RequestPipelineBuilderContext<TRequest, TResponse> CreatePipelineBuilderContext(RequestContext<TRequest> requestContext)
+        {
             var serviceProvider = requestContext.ServiceProvider;
 
-            var pipelineBuilderContext = new RequestPipelineBuilderContext<TRequest, TResponse>
+            return new RequestPipelineBuilderContext<TRequest, TResponse>
             {
                 Filters = serviceProvider.GetServices<IRequestFilter<TRequest, TResponse>>().ToListOptimized(),
                 HandlerExecutionStrategy = null,
                 Handlers = serviceProvider.GetServices<IRequestHandler<TRequest, TResponse>>().ToListOptimized()
             };
+        }
 
+        private async Task ExecutePipelineBuilders(RequestPipelineBuilderContext<TRequest, TResponse> pipelineBuilderContext, RequestContext<TRequest> requestContext)
+        {
             foreach (var pipelineBuilder in PipelineBuilders)
             {
                 await pipelineBuilder.Execute(pipelineBuilderContext, requestContext);
             }
-
-            AssertThereIsSingleHandlerToExecute(requestContext, pipelineBuilderContext.Handlers);
-
-            requestContext.CancellationToken.ThrowIfCancellationRequested();
-
-            var filters = pipelineBuilderContext.Filters;
-            var handler = pipelineBuilderContext.Handlers[0];
-            var handlerExecutionStrategy = pipelineBuilderContext.HandlerExecutionStrategy ?? MediatorDefaults.RequestHandlerExecutionStrategy;
-
-            return await Execute(filters, handler, handlerExecutionStrategy, requestContext).WithGloballyConfiguredAwait();
         }
 
-        private static void AssertThereIsSingleHandlerToExecute(RequestContext<TRequest> requestContext, List<IRequestHandler<TRequest, TResponse>> handlers)
+        private static void AssertThereIsSingleHandlerToExecute(List<IRequestHandler<TRequest, TResponse>> handlers)
         {
             if (handlers.Count == 0)
             {
-                throw new InvalidOperationException($"No handlers for request of type '{requestContext.Request.GetType().FullName}'.");
+                throw new InvalidOperationException($"No handlers for request of type '{typeof(TRequest).FullName}'.");
             }
 
             if (handlers.Count > 1)
             {
-                throw new InvalidOperationException($"Multiple handlers for request of type '{requestContext.Request.GetType().FullName}'.");
+                throw new InvalidOperationException($"Multiple handlers for request of type '{typeof(TRequest).FullName}'.");
             }
+        }
+
+        private async Task<TResponse> ExecutePipeline(
+            RequestPipelineBuilderContext<TRequest, TResponse> pipelineBuilderContext,
+            RequestContext<TRequest> requestContext)
+        {
+            requestContext.CancellationToken.ThrowIfCancellationRequested();
+
+            var filters = pipelineBuilderContext.Filters;
+            var handler = pipelineBuilderContext.Handlers[0];
+            var handlerExecutionStrategy = pipelineBuilderContext.HandlerExecutionStrategy ??
+                                           MediatorDefaults.RequestHandlerExecutionStrategy;
+
+            return await Execute(filters, handler, handlerExecutionStrategy, requestContext).WithGloballyConfiguredAwait();
         }
     }
 }
